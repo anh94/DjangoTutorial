a4d634dda9b0f9688975234842648a780f0664f2
anh94 <samedifriend@gmail.com>
2015-12-15 18:17:19 -0500

    I've fix my change on polls/admin.py
    I've made a mistake of class Question when
    I double type class QuestionAdmin(admin.ModelAdmin):
    I deleted one already. Then try to run the server again.

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
