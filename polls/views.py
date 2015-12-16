c6b64aa73390d166da2110a4c505d87f30a0657f
anh94 <samedifriend@gmail.com>
2015-12-16 15:34:06 -0500

    import timezone
    In the polls/views.py file, import timezone.

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from django.views import generic
-
+from django.utils import timezone
 
 from .models import Choice, Question
 
